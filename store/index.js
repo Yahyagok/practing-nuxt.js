079df4f8badb4e0772fcef0d39932463675cf3b2
Yahyagok <ygok17@hotmail.com>
2021-07-25 05:57:08 -0400

    some changes in the store

--- store/index.js
+++ store/index.js
@@ -179,14 +179,18 @@
           }
     ]
 })
+
 export const getters = { 
     getProductById: (state) => (id) => {
         return state.products.find(product => product.id == id)
     }
+
 }
+
 export const mutations = {
     addItem(state,id) {
         let item = state.products.find(product => product.id == id)
         state.myRentals.push(item)
     }
+    
 }
